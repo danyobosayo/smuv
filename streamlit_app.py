4401e910c79a39a78d32d1be0bbf4da6ed862a52
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2023-09-16 17:38:35 -0500

    Yayy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
+# streamlit_app.py
+
 import streamlit as st
 from st_files_connection import FilesConnection
-from googleapiclient.discovery import build
-from google.cloud import language_v1
+
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('gcs', type=FilesConnection)
@@ -10,20 +11,3 @@ df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
-
-youtube = build('youtube', 'v3', developerKey= 'YOUR_YOUTUBE_API_KEY')
-
-# Initialize Natural Language Processing API
-nlp_client = language_v1.LanguageServiceClient()
-
-# Streamlit app code
-st.title("YouTube and NLP Analysis")
-
-# Fetch YouTube video data
-video_data = youtube.videos().list(part='snippet,statistics', id='YOUR_VIDEO_ID').execute()
-# Process video data and display it using Streamlit widgets
-
-# Perform NLP analysis
-text_to_analyze = "Your text to analyze"
-document = language_v1.Document(content=text_to_analyze, type_=language_v1.Document.Type.PLAIN_TEXT)
-sentiment = nlp_client.analyze_sentiment(request={'document': document}).document_sentiment
