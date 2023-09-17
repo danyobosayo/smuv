531f00a6a383d02841cbd642813088c4d6b4fff3
duyenemily <duyenemily.nguyen@gmail.com>
2023-09-16 21:25:51 -0500

    fixed merge

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,7 @@ from st_files_connection import FilesConnection
 conn = st.experimental_connection('gcs', type=FilesConnection)
 df = conn.read("smuv-bucket/myfile.csv", input_format="csv", ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} loves :{row.Pet}:")
-
-# Input field for Youtube URL
-st.text_input('Enter some text')
-
-# I love cheeseburgers
-
-# Ring Ding Dang# streamlit_app.py
-import streamlit as st
-from st_files_connection import FilesConnection
-
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.experimental_connection('gcs', type=FilesConnection)
-df = conn.read("smuv-bucket/myfile.csv", input_format="csv", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} loves :{row.Pet}:")
+st.title("Youtube Channel Comment Trends")
 
 # Input field for Youtube URL
 url = st.text_input('Enter Youtube Channel URL')
